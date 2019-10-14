28087ee037e04e835b9d346fa3041ba569001bf5
lzl21 <56452461+lzl21@users.noreply.github.com>
2019-10-14 10:35:04 +0800

    Update and rename deploy_arm.ps1 to deploy_arm_template.ps1

--- deploy_arm.ps1
+++
@@ -1,7 +0,0 @@
-$resourceGroupName = "rg-la-test-sea-001"
-$location = "southeastasia"
-$templateFilePath = "acr_template.json"
-$parametersFilePath = "acr_parameters.json"
-
-New-AzResourceGroup -Name $resourceGroupName -Location $location
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath
